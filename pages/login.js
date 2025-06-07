06f623cc635c2a7ac54a5484109fe8372b868c94
Azzu <azzu@Azzus-Air.localdomain>
2025-06-07 16:22:34 +0200

    Your message describing the change

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { auth } from '../firebaseConfig';
+import { auth } from '/firebaseConfig';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
