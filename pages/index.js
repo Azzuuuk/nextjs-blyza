4bafb33d6d4cfd2388cebeedfa6e4a8c01864de4
Azzu <azzu@Azzus-Air.localdomain>
2025-06-16 22:35:12 +0200

    Your message describing the changggggge

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,25 @@ export default function GamesListPage() {
   return (
     <>
       <Head>
-        <title>Blyza - Powered by Fun, Driven by Rewards!</title>
-        <meta name="description" content="The party games platform that pays you back — with EXCLUSIVE OFFERS, PRIZES, and non-stop FUN!" />
-        <meta name="keywords" content="party games , Blyza , team building games, group games, online games with friends, fun games, browser games, find imposter, imposter games" />
-        <meta property="og:title" content="Blyza - Powered by Fun, Driven by Rewards!" />
-        <meta property="og:description" content="The party games platform that pays you back — with EXCLUSIVE OFFERS, PRIZES, and non-stop FUN!" />
-        <meta property="og:url" content="https://www.playblyza.com" />
-        <meta property="og:type" content="website" />
-        <meta name="robots" content="index, follow" />
+  <title>Blyza - Powered by Fun, Driven by Rewards!</title>
+  <meta name="description" content="The party games platform that pays you back — with EXCLUSIVE OFFERS, PRIZES, and non-stop FUN!" />
+  <meta name="keywords" content="party games , Blyza , team building games, group games, online games with friends, fun games, browser games, find imposter, imposter games" />
+  <meta property="og:title" content="Blyza - Powered by Fun, Driven by Rewards!" />
+  <meta property="og:description" content="The party games platform that pays you back — with EXCLUSIVE OFFERS, PRIZES, and non-stop FUN!" />
+  <meta property="og:url" content="https://www.playblyza.com" />
+  <meta property="og:type" content="website" />
+  <meta name="robots" content="index, follow" />
 
-         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+ <meta name='impact-site-verification' value='3739dacf-1165-45af-8389-3e63761bbc63' />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
   <link rel="manifest" href="/site.webmanifest" />
-  <link rel="icon" href="/favicon.ico" /> 
+  <link rel="icon" href="/favicon.ico" />
+</Head>
 
-      </Head>
 
       <iframe
         src="/games.html"
